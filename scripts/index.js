030089e84fd2c0b52d7b1507df1d1d2c7f9da3f0
jlau853 <64885702+jlau853@users.noreply.github.com>
2023-06-13 22:59:19 +1200

    Navigational Stuff

---
+++ scripts/index.js
@@ -0,0 +1,22 @@
+
+function navigation(){
+    const nav = document.querySelector('.nav')
+    const menu_button = document.querySelector('.menu_button')
+    if(nav.id === "nav_open"){
+        nav.id = "nav_close"
+        nav.style.visibility = "hidden"
+        menu_button.style.color = "black"
+        menu_button.style.backgroundColor = "white"
+        menu_button.style.border = "4px solid black"
+        menu_button.innerHTML = "Menu"
+        
+    }
+    else{
+        nav.id = "nav_open"
+        nav.style.visibility = "visible"
+        menu_button.style.color = "white"
+        menu_button.style.backgroundColor = "black"
+        menu_button.style.border = "4px solid white"
+        menu_button.innerHTML = "Close"
+    }
+}
